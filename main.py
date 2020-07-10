b5f8253c2454e00c0d6a4202bec13e1e401c3205
adarshkhanna <adarsh.khanna.14287@gmail.com>
2020-07-10 20:01:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Fundamental Data Types
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))  #0.5
+print(type(5.00001))
+print(type(9.9+1.1))
+print(2 ** 2)
+print(6 // 4)
+print(5 % 4)
